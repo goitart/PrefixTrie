f90352cf3c5fec6d7948bea9b1db986560705fbf
Danil <chevychelov06122002@gmail.com>
2022-03-03 19:22:50 +0300

    Initial commit

---
+++ src/main/java/Node.java
@@ -0,0 +1,9 @@
+//package trie;
+
+import java.util.*;
+
+public class Node {
+    String value;
+    HashMap<Character, Node> child;
+
+}
---
+++ src/main/java/Trie.java
@@ -0,0 +1,27 @@
+/*
+Вариант 13 -- префиксное дерево
+Хранит строки в виде префиксного дерева.
+Корневой узел такого дерева не хранит ничего,
+узлы 1-го уровня хранят первый символ строки,
+2-го уровня -- второй символ и так далее.
+Методы: добавление строки в дерево,
+удаление строки из дерева, поиск строки в дереве,
+поиск всех строк в дереве с заданным префиксом.
+*/
+//package trie;
+import java.util.*;
+
+import java.util.TreeMap;
+
+public class Trie {
+    public static void main(String[] args) {
+
+    }
+    Node root = new Node();
+    public void add(String string) {
+        if (string.isEmpty()){
+            return;
+        }
+//        if (next == null)
+    }
+}
